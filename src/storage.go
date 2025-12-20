1f1aa37328335ee9f858a539d2295b5021335525
Aarav Maloo <aaravmaloo06@gmail.com>
2025-12-20 21:58:42 +0530

    create storage.go for storage needs.

---
+++ src/storage.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"os"
+)
+
+func SaveVault(path string, data []byte) error {
+	return os.WriteFile(path, data, 0600)
+}
+
+func LoadVault(path string) ([]byte, error) {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return nil, err
+	}
+	return os.ReadFile(path)
+}
+
+func VaultExists(path string) bool {
+	_, err := os.Stat(path)
+	return !os.IsNotExist(err)
+}
