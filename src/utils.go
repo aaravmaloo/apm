22ceced63cf9beafe59226d570596b542b4634dd
Aarav Maloo <aaravmaloo06@gmail.com>
2026-01-03 15:36:01 +0530

    Add master password validation utility
    Introduces ValidateMasterPassword to enforce password strength requirements, including minimum length and presence of uppercase, lowercase, digit, and symbol characters. Also changes package name from main to apm.

--- src/utils.go
+++ src/utils.go
@@ -1,9 +1,11 @@
-package main
+package apm
 
 import (
 	"crypto/rand"
 	"encoding/base64"
+	"fmt"
 	"math/big"
+	"strings"
 
 	"golang.org/x/crypto/argon2"
 )
@@ -37,4 +39,39 @@ func EncodeBase64(b []byte) string {
 
 func DecodeBase64(s string) ([]byte, error) {
 	return base64.StdEncoding.DecodeString(s)
+}
+
+func ValidateMasterPassword(password string) error {
+	if len(password) < 8 {
+		return fmt.Errorf("password must be at least 8 characters long")
+	}
+
+	var hasUpper, hasLower, hasDigit, hasSymbol bool
+	for _, char := range password {
+		switch {
+		case 'A' <= char && char <= 'Z':
+			hasUpper = true
+		case 'a' <= char && char <= 'z':
+			hasLower = true
+		case '0' <= char && char <= '9':
+			hasDigit = true
+		case strings.ContainsRune("!@#$%^&*()-_=+", char):
+			hasSymbol = true
+		}
+	}
+
+	if !hasUpper {
+		return fmt.Errorf("password must contain at least one uppercase letter")
+	}
+	if !hasLower {
+		return fmt.Errorf("password must contain at least one lowercase letter")
+	}
+	if !hasDigit {
+		return fmt.Errorf("password must contain at least one digit")
+	}
+	if !hasSymbol {
+		return fmt.Errorf("password must contain at least one symbol (!@#$%%^&*()-_=+)")
+	}
+
+	return nil
 }
