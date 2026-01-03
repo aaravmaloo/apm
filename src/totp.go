8946577ec3c8b2ddea38c2a17d7bc8ea6c88c5d7
Aarav Maloo <aaravmaloo06@gmail.com>
2026-01-03 15:15:27 +0530

    added totp
    extending support of apm to time based one time password

---
+++ src/totp.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"time"
+
+	"github.com/pquerna/otp/totp"
+)
+
+func GenerateTOTP(secret string) (string, error) {
+	return totp.GenerateCode(secret, time.Now())
+}
+
+func TimeRemaining() int {
+	return 30 - (int(time.Now().Unix()) % 30)
+}
