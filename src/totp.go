0cde490450642fc5c9393ee265fe8a1c012ca76e
Aarav Maloo <aaravmaloo06@gmail.com>
2026-01-06 23:00:36 +0530

    update totp

--- src/totp.go
+++ src/totp.go
@@ -1,12 +1,15 @@
 package apm
 
 import (
+	"strings"
 	"time"
 
 	"github.com/pquerna/otp/totp"
 )
 
 func GenerateTOTP(secret string) (string, error) {
+	secret = strings.ReplaceAll(secret, " ", "")
+	secret = strings.ToUpper(secret)
 	return totp.GenerateCode(secret, time.Now())
 }
 
