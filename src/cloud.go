18b5cd6ee0204eb409de0d446e002e80058b3308
Aarav Maloo <aaravmaloo06@gmail.com>
2026-01-11 18:00:56 +0530

    remove comments

--- src/cloud.go
+++ src/cloud.go
@@ -8,7 +8,6 @@ import (
 	"io"
 	"net/http"
 	"os"
-	"path/filepath"
 	"time"
 
 	"github.com/mr-tron/base58/base58"
@@ -20,7 +19,7 @@ import (
 
 const DriveFolderID = "100G-gs-wQnjmGipXdKFBBNd-Qbu6PpYJ"
 
-// GenerateRetrievalKey creates a unique Base58 retrieval key for a vault.
+
 func GenerateRetrievalKey() (string, error) {
 	b := make([]byte, 16)
 	if _, err := rand.Read(b); err != nil {
@@ -29,45 +28,34 @@ func GenerateRetrievalKey() (string, error) {
 	return base58.Encode(b), nil
 }
 
-// CloudManager handles Google Drive operations.
+
 type CloudManager struct {
 	Service *drive.Service
 }
 
-// NewCloudManager initializes a new CloudManager.
-func NewCloudManager(ctx context.Context, credentialsPath string) (*CloudManager, error) {
-	// Look for token.json in the same directory as credentials.json
-	baseDir := filepath.Dir(credentialsPath)
-	tokenPath := filepath.Join(baseDir, "token.json")
 
-	if _, err := os.Stat(credentialsPath); os.IsNotExist(err) {
-		return nil, fmt.Errorf("credentials file not found at %s", credentialsPath)
+func NewCloudManager(ctx context.Context, credsJSON []byte, tokenJSON []byte) (*CloudManager, error) {
+	if len(credsJSON) == 0 {
+		return nil, fmt.Errorf("cloud credentials missing")
 	}
 
-	b, err := os.ReadFile(credentialsPath)
-	if err != nil {
-		return nil, fmt.Errorf("unable to read credentials: %v", err)
-	}
-
-	config, err := google.ConfigFromJSON(b, drive.DriveFileScope)
+	config, err := google.ConfigFromJSON(credsJSON, drive.DriveFileScope)
 	if err != nil {
 		return nil, fmt.Errorf("unable to parse config: %v", err)
 	}
 
-	var srv *drive.Service
-	if _, err := os.Stat(tokenPath); err == nil {
-		// Use existing token.json
-		f, _ := os.Open(tokenPath)
-		defer f.Close()
+	var client *http.Client
+	if len(tokenJSON) > 0 {
 		tok := &oauth2.Token{}
-		json.NewDecoder(f).Decode(tok)
-		client := config.Client(ctx, tok)
-		srv, err = drive.NewService(ctx, option.WithHTTPClient(client))
+		if err := json.Unmarshal(tokenJSON, tok); err != nil {
+			return nil, fmt.Errorf("unable to parse token: %v", err)
+		}
+		client = config.Client(ctx, tok)
 	} else {
-		// Fallback to local file (might trigger standard flow if supported by library or fail)
-		srv, err = drive.NewService(ctx, option.WithCredentialsFile(credentialsPath))
+		return nil, fmt.Errorf("cloud token missing")
 	}
 
+	srv, err := drive.NewService(ctx, option.WithHTTPClient(client))
 	if err != nil {
 		return nil, fmt.Errorf("unable to retrieve Drive client: %v", err)
 	}
@@ -75,7 +63,7 @@ func NewCloudManager(ctx context.Context, credentialsPath string) (*CloudManager
 	return &CloudManager{Service: srv}, nil
 }
 
-// UploadVault uploads the vault to Google Drive and makes it public.
+
 func (cm *CloudManager) UploadVault(vaultPath string) (string, error) {
 	f, err := os.Open(vaultPath)
 	if err != nil {
@@ -83,7 +71,7 @@ func (cm *CloudManager) UploadVault(vaultPath string) (string, error) {
 	}
 	defer f.Close()
 
-	// Initial naming to something random
+
 	driveFile := &drive.File{
 		Name:    fmt.Sprintf("vault_pending_%d.bin", time.Now().Unix()),
 		Parents: []string{DriveFolderID},
@@ -96,7 +84,7 @@ func (cm *CloudManager) UploadVault(vaultPath string) (string, error) {
 
 	fileID := res.Id
 
-	// Make it public
+
 	permission := &drive.Permission{
 		Type: "anyone",
 		Role: "reader",
@@ -106,7 +94,7 @@ func (cm *CloudManager) UploadVault(vaultPath string) (string, error) {
 		return "", fmt.Errorf("unable to make file public: %v", err)
 	}
 
-	// Rename it to deterministic name: vault_<fileID>.bin
+
 	newName := fmt.Sprintf("vault_%s.bin", fileID)
 	_, err = cm.Service.Files.Update(fileID, &drive.File{Name: newName}).Do()
 	if err != nil {
@@ -116,7 +104,7 @@ func (cm *CloudManager) UploadVault(vaultPath string) (string, error) {
 	return fileID, nil
 }
 
-// DownloadVault downloads the vault using OAuth (for verified uploader).
+
 func (cm *CloudManager) DownloadVault(fileID string) ([]byte, error) {
 	resp, err := cm.Service.Files.Get(fileID).Download()
 	if err != nil {
@@ -127,7 +115,7 @@ func (cm *CloudManager) DownloadVault(fileID string) ([]byte, error) {
 	return io.ReadAll(resp.Body)
 }
 
-// DownloadPublicVault downloads a publicly available vault without OAuth directly from Drive.
+
 func DownloadPublicVault(fileID string) ([]byte, error) {
 	url := fmt.Sprintf("https://drive.google.com/uc?export=download&id=%s", fileID)
 	resp, err := http.Get(url)
@@ -143,7 +131,7 @@ func DownloadPublicVault(fileID string) ([]byte, error) {
 	return io.ReadAll(resp.Body)
 }
 
-// SyncVault updates the existing cloud file.
+
 func (cm *CloudManager) SyncVault(vaultPath, fileID string) error {
 	f, err := os.Open(vaultPath)
 	if err != nil {
@@ -159,7 +147,7 @@ func (cm *CloudManager) SyncVault(vaultPath, fileID string) error {
 	return nil
 }
 
-// DeleteVault deletes the vault from Google Drive.
+
 func (cm *CloudManager) DeleteVault(fileID string) error {
 	err := cm.Service.Files.Delete(fileID).Do()
 	if err != nil {
@@ -168,7 +156,7 @@ func (cm *CloudManager) DeleteVault(fileID string) error {
 	return nil
 }
 
-// ListVaults lists all vaults in the Drive folder.
+
 func (cm *CloudManager) ListVaults() ([]string, error) {
 	query := fmt.Sprintf("'%s' in parents and trashed = false", DriveFolderID)
 	list, err := cm.Service.Files.List().Q(query).Do()
