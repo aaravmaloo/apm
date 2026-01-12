660de4124a0a69a3e09623552b6a890dd17e1ebe
Aarav Maloo <aaravmaloo06@gmail.com>
2026-01-12 19:22:34 +0530

    remove comments

--- tests/team_verify.go
+++ tests/team_verify.go
@@ -9,11 +9,11 @@ import (
 func main() {
 	fmt.Println("Starting Team APM Verification...")
 
-	// 1. Clean up existing vault
+	
 	os.Remove("team_vault.dat")
 	os.Remove("session.json")
 
-	// 2. Build the team binary
+	
 	fmt.Println("Building team binary...")
 	buildCmd := exec.Command("go", "build", "-o", "pm-team-test.exe", "main.go", "team_vault.go", "session.go", "crypto.go")
 	buildCmd.Dir = "./team"
