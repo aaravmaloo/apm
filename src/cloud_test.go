6d10aac04f1ee234eb518f0ad766fff9079bd238
Aarav Maloo <aaravmaloo06@gmail.com>
2026-02-08 22:32:19 +0530

    Create cloud_test.go

---
+++ src/cloud_test.go
@@ -0,0 +1,58 @@
+package apm
+
+import (
+	"testing"
+)
+
+func TestExtractFileID(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{
+			name:     "Raw ID",
+			input:    "100G-gs-wQnjmGipXdKFBBNd-Qbu6PpYJ",
+			expected: "100G-gs-wQnjmGipXdKFBBNd-Qbu6PpYJ",
+		},
+		{
+			name:     "Standard View Link",
+			input:    "https://drive.google.com/file/d/100G-gs-wQnjmGipXdKFBBNd-Qbu6PpYJ/view?usp=sharing",
+			expected: "100G-gs-wQnjmGipXdKFBBNd-Qbu6PpYJ",
+		},
+		{
+			name:     "Link with query params",
+			input:    "https://drive.google.com/file/d/100G-gs-wQnjmGipXdKFBBNd-Qbu6PpYJ?usp=sharing",
+			expected: "100G-gs-wQnjmGipXdKFBBNd-Qbu6PpYJ",
+		},
+		{
+			name:     "Export Link format",
+			input:    "https://drive.google.com/uc?id=100G-gs-wQnjmGipXdKFBBNd-Qbu6PpYJ&export=download",
+			expected: "100G-gs-wQnjmGipXdKFBBNd-Qbu6PpYJ",
+		},
+		{
+			name:     "Export Link format simple",
+			input:    "https://drive.google.com/uc?export=download&id=100G-gs-wQnjmGipXdKFBBNd-Qbu6PpYJ",
+			expected: "100G-gs-wQnjmGipXdKFBBNd-Qbu6PpYJ",
+		},
+		{
+			name:     "Empty Input",
+			input:    "",
+			expected: "",
+		},
+		{
+			name:     "No ID found",
+			input:    "https://google.com",
+			expected: "https://google.com", // Or however the function behaves on failure, arguably it returns input or empty
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := ExtractFileID(tt.input)
+			if got != tt.expected {
+				t.Errorf("ExtractFileID(%q) = %v, want %v", tt.input, got, tt.expected)
+			}
+		})
+	}
+}
