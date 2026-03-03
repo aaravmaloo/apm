0bccb530ea4b77eaddafbd83f5428b6aae0a977a
Aarav Maloo <aaravmaloo06@gmail.com>
2026-03-03 13:57:49 +0530

    chore: update src/tui/totp.go

---
+++ src/tui/totp.go
@@ -0,0 +1,25 @@
+package tui
+
+import (
+	"fmt"
+	src "password-manager/src"
+)
+
+func RenderTOTPList(vault *src.Vault) string {
+	var s string
+	s += TitleStyle.Render(" TOTP Accounts ") + "\n\n"
+
+	if len(vault.TOTPEntries) == 0 {
+		s += "No TOTP accounts found.\n"
+	} else {
+		for _, e := range vault.TOTPEntries {
+			code, err := src.GenerateTOTP(e.Secret)
+			if err != nil {
+				code = "INVALID"
+			}
+			s += fmt.Sprintf("%-20s | %s\n", e.Account, code)
+		}
+	}
+
+	return BorderStyle.Render(s)
+}
