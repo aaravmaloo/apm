e2c7a114abbb174c375eb7448608519aeae7e6aa
Aarav Maloo <aaravmaloo06@gmail.com>
2026-03-03 13:57:44 +0530

    chore: update src/autofill/paths.go

---
+++ src/autofill/paths.go
@@ -0,0 +1,34 @@
+package autofill
+
+import (
+	"os"
+	"path/filepath"
+)
+
+func autofillDir() (string, error) {
+	configDir, err := os.UserConfigDir()
+	if err != nil {
+		return "", err
+	}
+	dir := filepath.Join(configDir, "apm", "autofill")
+	if err := os.MkdirAll(dir, 0700); err != nil {
+		return "", err
+	}
+	return dir, nil
+}
+
+func stateFilePath() (string, error) {
+	dir, err := autofillDir()
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(dir, "daemon_state.json"), nil
+}
+
+func profileFilePath() (string, error) {
+	dir, err := autofillDir()
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(dir, "profiles.json"), nil
+}
